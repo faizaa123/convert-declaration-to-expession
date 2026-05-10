273fa26818a2430705c5c08c9ae172fc60e27b28
sohana saeed <sohanasaeed61@gmail.com>
2026-05-10 15:25:57 +0500

    expressions

---
+++ app.js
@@ -0,0 +1,14 @@
+
+let add = function(a,b){
+    return a +b 
+}
+ console.log(add(8,8));
+
+let greet = function(name){
+    return`hello ${name}` 
+}
+ console.log(greet("faiza"));
+let square = function(x){
+    return x * x;
+}
+ console.log(square(4));
